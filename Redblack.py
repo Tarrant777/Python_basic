669f38d2e9cce7e61562e2f6156f4f279be983d2
Tarrant777 <linustin@gmail.com>
2018-06-01 13:23:10 +0800

    紅黑一半

--- Redblack.py
+++ Redblack.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 import random
 
-i
 
 randomtimes = 100
 # 隨機次數
@@ -10,20 +9,28 @@ numbers = 1
 d = {}
 # 產生對應字典
 
+payback = 0.95
+
 Total_property = 0
 Bet = [1, 3, 9, 27, 81, 243, 729]  # 下注值
 
 Result = []  # 紅黑輸贏結果
 
+
+def game()
+
+
 for j in range(0, numbers + 1):
     Result = []  # 歸零
     for i in range(0, randomtimes + 1):
+
         a = random.randint(0, 999)
         if a <= 499:
-
             Result.append(1)
         else:
             Result.append(0)
+        if i > 3 and Result(i - 1) == Result(i - 2) and Result(i - 1) == Result(i - 3):
+            if Result(i-1)==1 and Result(i)== 1:
+
+
     d[j] = Result  # 把j號樣本放入字典
-
-
